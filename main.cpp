4583089b55d57caf0389261bb870ff71c9b1275e
Manuel Meyer <manuel.meyer.extern@googlemail.com>
2020-10-27 22:50:03 +0100

    test set motor functionality in test program.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <unistd.h>
 #include "include/ThunderBorg.hpp"
 
 int main(int argc, char **argv) {
     passbutter::ThunderBorg thunderborg("test");
     
     std::vector<int> boardAddrs = thunderborg.detectBoards(1);
+    if (boardAddrs.size() == 0)
+    {
+        std::cout << "no boards detected.." << std::endl;
+    }
+    
+    thunderborg.initBus(boardAddrs[0]);
+    
+    thunderborg.setMotor1(0.3);
+    usleep(1000);
+    thunderborg.setMotor1(0);
     
     std::cout << "Hello, world!" << std::endl;
     return 0;
